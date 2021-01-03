42f08c373bd90441569fa689f1f51d010b0aa843
Skylord <skylord475@gmail.com>
2021-01-03 10:43:31 +0100

    Create year.py

---
+++ year.py
@@ -0,0 +1,64 @@
+# @skylord, if you like you can hit the upvote
+        
+name = input("wishings you HAPPY NEW YEAR: \n\n\n")
+
+for na in name:
+    na = na.upper()
+    if (na == "A"):
+       print("                        😍 \n                     😍   😍\n                     😍___😍\n                     😍   😍\n                     😍   😍\n\n\n")
+    elif (na =="B"):
+       print("2")
+    elif (na =="C"):
+       print("3")
+    elif (na =="D"):
+       print("4")
+    elif (na =="E"):
+       print("                     💛💛💛💛\n                     💛     \n                     💛💛💛💛\n                     💛    \n                     💛💛💛💛\n\n\n")
+    elif (na =="F"):
+       print("6")
+    elif (na =="G"):
+       print("7")
+    elif (na =="H"):
+       print("                     💚    💚\n                     💚    💚\n                     💚 💚 💚\n                     💚    💚\n                     💚    💚\n\n\n")
+    elif (na =="I"):
+       print("9")
+    elif (na =="J"):
+       print("10")
+    elif (na =="K"):
+       print("11")
+    elif (na =="L"):
+       print("12")
+    elif (na =="M"):
+       print("13")
+    elif (na =="N"):
+       print("                          💙  💙\n                        💙 💙 💙\n                       💙  💙 💙\n 💙   💙 💙\n                     💙    💙💙\n\n\n")
+    elif (na =="O"):
+       print("15")
+    elif (na =="P"):
+       print("                     💜💜💜\n                     💜  💜\n                     💜💜💜\n                     💜\n                     💜\n\n")
+    elif (na =="Q"):
+       print("17")
+    elif (na =="R"):
+       print("                     🤎🤎🤎🤎\n                     🤎    🤎 \n                     🤎   🤎  \n                     🤎🤎🤎\n                     🤎  🤎\n                     🤎   🤎\n                     🤎    🤎\n\n\n")
+    elif (na =="S"):
+       print("19")
+    elif (na =="T"):
+       print("20")
+    elif (na =="U"):
+       print("21")
+    elif (na =="V"):
+       print("22")
+    elif (na =="W"):
+       print("                     ♥️         ♥️\n                     ♥️   ♥️    ♥️\n                     ♥️  ♥️ ♥️  ♥️\n                     ♥️ ♥️   ♥️ ♥️\n                     ♥️♥️     ♥️♥️\n\n\n")
+    elif (na =="X"):
+       print("24")
+    elif (na =="Y"):
+       print("                     🤍  🤍\n                      🤍‎‎‎‎‎ 🤍\n                       🤍🤍\n                         🤍\n                         🤍\n\n\n")
+    elif (na =="Z"):
+       print("26")
+    elif (na ==" "):
+       print(" \n\n\n\n")
+# @skylord
+print("_happy_new_year___greeting_from_SKYLORD___")
+print("_______________________________________")
+print("love you guys!!!!!!!!")
